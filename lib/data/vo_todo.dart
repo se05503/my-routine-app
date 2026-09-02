65cbcf44f1ab717b8548643dd8d062aa84f6b755
se05503 <seyeongb18@gmail.com>
2026-09-02 15:56:42 +0900

    feat: 할일 모델 정의 - 제목, 설명, 마감일

---
+++ lib/data/vo_todo.dart
@@ -0,0 +1,7 @@
+class TodoItem {
+  final String title;
+  final String? description;
+  final DateTime? deadline;
+
+  TodoItem({required this.deadline, required this.title, required this.description});
+}
