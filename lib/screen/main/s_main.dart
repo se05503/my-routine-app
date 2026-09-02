5beca4adcf9b7303012cb98fb9e9a7e377322c9b
se05503 <seyeongb18@gmail.com>
2026-09-02 16:00:46 +0900

    refactor: fab 로직 분리 - 기존: 할일, 검색을 포함하는 메인 스크린의 Scaffold - 이후: 할일 프래그먼트의 Scaffold

--- lib/screen/main/s_main.dart
+++ lib/screen/main/s_main.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:my_routine_app/common/datetime_extension.dart';
 import 'package:my_routine_app/screen/main/search/f_search.dart';
 import 'package:my_routine_app/screen/main/todo/f_todo.dart';
-
-import '../widget/w_rounded_container.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
@@ -15,17 +12,12 @@ class MainScreen extends StatefulWidget {
 class _MainScreenState extends State<MainScreen> {
   int _selectedIndex = 0;
   static const List<Widget> _screens = [TodoFragment(), SearchFragment()];
-  DateTime _selectedDate = DateTime.now();
-  bool _isDateSelected = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme
-            .of(context)
-            .colorScheme
-            .inversePrimary,
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text('My Routine'),
       ),
       body: Center(child: _screens[_selectedIndex]),
@@ -44,110 +36,6 @@ class _MainScreenState extends State<MainScreen> {
             _selectedIndex = index;
           });
         },
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          showModalBottomSheet(
-            context: context,
-            builder: (context) {
-              return Padding(
-                padding: EdgeInsets.only(
-                  bottom: MediaQuery
-                      .of(context)
-                      .viewInsets
-                      .bottom,
-                ),
-                child: RoundedContainer(
-                  radius: 20,
-                  padding: EdgeInsets.symmetric(horizontal: 12, vertical: 10),
-                  color: Theme
-                      .of(context)
-                      .canvasColor,
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      TextField(
-                        autofocus: true,
-                        decoration: InputDecoration(
-                          hintText: "무엇을 하고 싶으신가요?",
-                          border: InputBorder.none,
-                          isDense: true,
-                          contentPadding: EdgeInsets.zero,
-                        ),
-                      ),
-                      SizedBox(height: 6),
-                      TextField(
-                        decoration: InputDecoration(
-                          hintText: "설명",
-                          border: InputBorder.none,
-                          isDense: true,
-                          contentPadding: EdgeInsets.zero,
-                        ),
-                      ),
-                      Row(
-                        children: [
-                          IconButton(
-                            onPressed: () async {
-                              final date = await showDatePicker(context: context,
-                                  initialDate: _selectedDate,
-                                  firstDate: DateTime.now(),
-                                  lastDate: DateTime.now().add(Duration(days: 365)));
-                              if(date!=null) {
-                                setState(() {
-                                  _isDateSelected = true;
-                                  _selectedDate = date;
-                                });
-                              }
-                            },
-                            icon: Icon(Icons.calendar_month),
-                            padding: EdgeInsets.zero,
-                            // 패딩 제거
-                            constraints: const BoxConstraints(),
-                            // 최소 크기 제약 제거
-                            visualDensity: VisualDensity.compact,
-                            // 시각적 밀도 압축
-                            style: IconButton.styleFrom(
-                              tapTargetSize: MaterialTapTargetSize.shrinkWrap,
-                            ),
-                          ),
-                          SizedBox(width: 6),
-                          if(_isDateSelected) Text(_selectedDate.formattedDate),
-                          IconButton(
-                            onPressed: () {},
-                            icon: Icon(Icons.flag),
-                            padding: EdgeInsets.zero,
-                            // 패딩 제거
-                            constraints: const BoxConstraints(),
-                            // 최소 크기 제약 제거
-                            visualDensity: VisualDensity.compact,
-                            // 시각적 밀도 압축
-                            style: IconButton.styleFrom(
-                              tapTargetSize: MaterialTapTargetSize.shrinkWrap,
-                            ),
-                          ),
-                          Spacer(),
-                          FilledButton(
-                            onPressed: () {},
-                            child: Text("추가"),
-                            style: FilledButton.styleFrom(
-                              minimumSize: Size.zero,
-                              padding: const EdgeInsets.symmetric(
-                                horizontal: 16,
-                                vertical: 8,
-                              ),
-                              tapTargetSize: MaterialTapTargetSize.shrinkWrap,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
-              );
-            },
-          );
-        },
-        child: const Icon(Icons.add),
       ),
     );
   }
--- lib/screen/main/todo/f_todo.dart
+++ lib/screen/main/todo/f_todo.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import '../../../common/datetime_extension.dart';
+import '../../../data/vo_todo.dart';
+import '../../widget/w_rounded_container.dart';
 
 class TodoFragment extends StatefulWidget {
   const TodoFragment({super.key});
@@ -8,8 +11,117 @@ class TodoFragment extends StatefulWidget {
 }
 
 class _TodoFragmentState extends State<TodoFragment> {
+  DateTime? _selectedDate;
+  final _titleController = TextEditingController();
+  final _descriptionController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Placeholder(),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            showModalBottomSheet(
+              context: context,
+              builder: (context) {
+                return Padding(
+                  padding: EdgeInsets.only(
+                    bottom: MediaQuery.of(context).viewInsets.bottom,
+                  ),
+                  child: RoundedContainer(
+                    radius: 20,
+                    padding: EdgeInsets.symmetric(horizontal: 12, vertical: 10),
+                    color: Theme.of(context).canvasColor,
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        TextField(
+                          autofocus: true,
+                          controller: _titleController,
+                          decoration: InputDecoration(
+                            hintText: "무엇을 하고 싶으신가요?",
+                            border: InputBorder.none,
+                            isDense: true,
+                            contentPadding: EdgeInsets.zero,
+                          ),
+                        ),
+                        SizedBox(height: 6),
+                        TextField(
+                          controller: _descriptionController,
+                          decoration: InputDecoration(
+                            hintText: "설명",
+                            border: InputBorder.none,
+                            isDense: true,
+                            contentPadding: EdgeInsets.zero,
+                          ),
+                        ),
+                        Row(
+                          children: [
+                            IconButton(
+                              onPressed: () async {
+                                final date = await showDatePicker(
+                                  context: context,
+                                  initialDate: _selectedDate ?? DateTime.now(),
+                                  firstDate: DateTime.now().subtract(
+                                    Duration(days: 14),
+                                  ),
+                                  lastDate: DateTime.now().add(
+                                    Duration(days: 365),
+                                  ),
+                                );
+                                if (date != null) {
+                                  setState(() {
+                                    _selectedDate = date;
+                                  });
+                                }
+                              },
+                              icon: Icon(Icons.calendar_month),
+                              padding: EdgeInsets.zero,
+                              constraints: const BoxConstraints(),
+                              visualDensity: VisualDensity.compact,
+                              style: IconButton.styleFrom(
+                                tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                              ),
+                            ),
+                            SizedBox(width: 6),
+                            if (_selectedDate != null)
+                              Text(_selectedDate!.formattedDate),
+                            IconButton(
+                              onPressed: () {},
+                              icon: Icon(Icons.flag),
+                              padding: EdgeInsets.zero,
+                              constraints: const BoxConstraints(),
+                              visualDensity: VisualDensity.compact,
+                              style: IconButton.styleFrom(
+                                tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                              ),
+                            ),
+                            Spacer(),
+                            FilledButton(
+                              onPressed: () {
+
+                              },
+                              style: FilledButton.styleFrom(
+                                minimumSize: Size.zero,
+                                padding: const EdgeInsets.symmetric(
+                                  horizontal: 16,
+                                  vertical: 8,
+                                ),
+                                tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                              ),
+                              child: Text("추가"),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            );
+          },
+          child: const Icon(Icons.add),
+        ),
+    );
   }
 }
