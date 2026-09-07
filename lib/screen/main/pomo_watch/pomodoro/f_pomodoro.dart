f354ebbfad6103b2f83d4ef9226273bbe97d40c4
se05503 <seyeongb18@gmail.com>
2026-09-07 21:49:59 +0900

    design: 포모도로 화면 UI - 할일 선택 텍스트 - 남은 시간 및 시간이 줄어드는걸 시각적으로 표현하는 CircularProgressIndicator 위젯 - 재생 및 멈춤 아이콘 버튼

--- lib/screen/main/pomo_watch/pomodoro/f_pomodoro.dart
+++ lib/screen/main/pomo_watch/pomodoro/f_pomodoro.dart
@@ -10,6 +10,58 @@ class PomodoroFragment extends StatefulWidget {
 class _PomodoroFragmentState extends State<PomodoroFragment> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [Text("할일 선택"), Icon(Icons.keyboard_arrow_right_rounded)],
+        ),
+        Stack(
+          alignment: Alignment.center,
+          children: [
+            SizedBox(
+              width: 250,
+              height: 250,
+              child: CircularProgressIndicator(
+                value: 1.0,
+                strokeWidth: 10,
+                color: Colors.grey[300],
+              ),
+            ),
+            SizedBox(
+              width: 250,
+              height: 250,
+              child: CircularProgressIndicator(
+                value: 0.7,
+                strokeWidth: 10,
+                color: Colors.blue,
+                strokeCap: StrokeCap.round,
+              ),
+            ),
+            const Text(
+              "60:00",
+              style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
+        Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.play_circle_fill_rounded),
+              iconSize: 40,
+            ),
+            SizedBox(width: 20,),
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.stop_circle_rounded),
+              iconSize: 40,
+            ),
+          ],
+        ),
+      ],
+    );
   }
 }
