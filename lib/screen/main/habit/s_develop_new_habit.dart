03d95c0b94d9d3e1fd1fa67ffb46a46d29ec7b03
se05503 <seyeongb18@gmail.com>
2026-09-20 17:38:48 +0900

    feat: 아이콘 검색 기능 - Icons8 CDN 아이콘 검색이 영어만 가능함 → MyMemory API를 연동해 한글을 영어로 변환 - http 패키지 추가 - 사용자가 한글로 아이콘 검색 → MyMemory API로 영어로 변환 → Icons CDN으로부터 아이콘 이미지를 가져오기

--- lib/screen/main/habit/s_develop_new_habit.dart
+++ lib/screen/main/habit/s_develop_new_habit.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_colorpicker/flutter_colorpicker.dart';
 import 'package:my_routine_app/screen/widget/w_rounded_container.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 class DevelopNewHabitScreen extends StatefulWidget {
   const DevelopNewHabitScreen({super.key});
@@ -22,6 +24,7 @@ class _DevelopNewHabitScreenState extends State<DevelopNewHabitScreen> {
     Colors.purple.shade400,
   ];
   Color? selectedColor;
+  String iconName = "search";
 
   @override
   Widget build(BuildContext context) {
@@ -32,123 +35,141 @@ class _DevelopNewHabitScreenState extends State<DevelopNewHabitScreen> {
         // 만약 수동으로 아이콘을 지정하고 싶다면 leading 속성을 사용한다.
         title: Text("새로운 습관"),
       ),
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 16, vertical: 20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch, // 자식들을 가로로 꽉 채움
-          children: [
-            RoundedContainer(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text("이름"),
-                  SizedBox(height: 8),
-                  _buildTextField(
-                    controller: _nameController,
-                    hintText: "일찍 일어나기",
-                  ),
-                ],
-              ),
-            ),
-            SizedBox(height: 16),
-            RoundedContainer(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text("설명"),
-                  SizedBox(height: 8),
-                  _buildTextField(
-                    controller: _descriptionController,
-                    hintText: "하루를 기분좋게 시작할 수 있어요",
-                  ),
-                ],
-              ),
-            ),
-            SizedBox(height: 16),
-            RoundedContainer(
-              padding: EdgeInsets.symmetric(horizontal: 16, vertical: 12),
-              color: Colors.white,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text("아이콘 / 이미지"),
-                  SizedBox(height: 8),
-                  Row(
-                    children: [
-                      CircleAvatar(
-                        backgroundColor: selectedColor ?? Colors.transparent,
-                        child: Image.network(
-                          width: 30,
-                          height: 30,
-                          "https://img.icons8.com/color/96/yoga.png",
-                          errorBuilder: (context, error, stackTrace) {
-                            return Icon(Icons.error);
-                          },
-                        ),
-                      ),
-                      SizedBox(width: 8),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [Text("선택된 아이콘"), Text("요가")],
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: 8),
-                  TextField(
-                    controller: _iconNameController,
-                    decoration: InputDecoration(
-                      hintText: "아이콘 이름으로 검색 (예: 산책, 독서)",
-                      hintStyle: TextStyle(color: Colors.grey.shade400),
-                      filled: true,
-                      fillColor: Colors.grey[200],
-                      border: InputBorder.none,
-                      enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(8),
-                        borderSide: BorderSide.none, // 테두리 선 제거
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(8),
-                        borderSide: BorderSide.none, // 테두리 선 제거
-                      ),
-                      prefixIcon: Icon(Icons.search),
-                      isDense: true,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16, vertical: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch, // 자식들을 가로로 꽉 채움
+            children: [
+              RoundedContainer(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("이름"),
+                    SizedBox(height: 8),
+                    _buildTextField(
+                      controller: _nameController,
+                      hintText: "일찍 일어나기",
                     ),
-                    style: TextStyle(color: Colors.black),
-                  ),
-                  SizedBox(height: 8),
-                  Text("색상"),
-                  SizedBox(height: 8),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      _buildNoneColorItem(),
-                      ...colorPresets.map(
-                        (color) => _buildColorPresetItem(color),
+                  ],
+                ),
+              ),
+              SizedBox(height: 16),
+              RoundedContainer(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("설명"),
+                    SizedBox(height: 8),
+                    _buildTextField(
+                      controller: _descriptionController,
+                      hintText: "하루를 기분좋게 시작할 수 있어요",
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 16),
+              RoundedContainer(
+                padding: EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+                color: Colors.white,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("아이콘 / 이미지"),
+                    SizedBox(height: 8),
+                    Row(
+                      children: [
+                        CircleAvatar(
+                          backgroundColor: selectedColor ?? Colors.transparent,
+                          child: Image.network(
+                            width: 30,
+                            height: 30,
+                            "https://img.icons8.com/color/96/$iconName.png",
+                            errorBuilder: (context, error, stackTrace) {
+                              return Icon(Icons.error);
+                            },
+                          ),
+                        ),
+                        SizedBox(width: 8),
+                         Text("선택된 아이콘")
+                      ],
+                    ),
+                    SizedBox(height: 8),
+                    TextField(
+                      controller: _iconNameController,
+                      decoration: InputDecoration(
+                        hintText: "아이콘 이름으로 검색 (예: 산책, 독서)",
+                        hintStyle: TextStyle(color: Colors.grey.shade400),
+                        filled: true,
+                        fillColor: Colors.grey[200],
+                        border: InputBorder.none,
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8),
+                          borderSide: BorderSide.none, // 테두리 선 제거
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8),
+                          borderSide: BorderSide.none, // 테두리 선 제거
+                        ),
+                        prefixIcon: IconButton(
+                          onPressed: () {
+                            setState(() async {
+                              final translatedIconName =
+                                  await translateToEnglish(
+                                    _iconNameController.text,
+                                  );
+                              if (translatedIconName != null) {
+                                setState(() {
+                                  iconName = translatedIconName;
+                                });
+                              } else {
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                  SnackBar(content: Text("알 수 없는 에러가 발생했습니다.")), // 한글 → 영어 변환 과정에서의 에러
+                                );
+                              }
+                            });
+                          },
+                          icon: Icon(Icons.search),
+                        ),
+                        isDense: true,
                       ),
-                      _buildColorPickerItem(),
-                    ],
-                  ),
-                ],
+                      style: TextStyle(color: Colors.black),
+                    ),
+                    SizedBox(height: 8),
+                    Text("색상"),
+                    SizedBox(height: 8),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        _buildNoneColorItem(),
+                        ...colorPresets.map(
+                          (color) => _buildColorPresetItem(color),
+                        ),
+                        _buildColorPickerItem(),
+                      ],
+                    ),
+                  ],
+                ),
               ),
-            ),
-            SizedBox(height: 16),
-            RoundedContainer(
-              padding: EdgeInsets.symmetric(horizontal: 16, vertical: 12),
-              color: Colors.white,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [Text("빈도")],
+              SizedBox(height: 16),
+              RoundedContainer(
+                padding: EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+                color: Colors.white,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [Text("빈도")],
+                ),
               ),
-            ),
-            SizedBox(height: 16),
-            FilledButton(
-              onPressed: () {},
-              style: ButtonStyle(
-                backgroundColor: WidgetStateProperty.all(Colors.black),
+              SizedBox(height: 16),
+              FilledButton(
+                onPressed: () {},
+                style: ButtonStyle(
+                  backgroundColor: WidgetStateProperty.all(Colors.black),
+                ),
+                child: Text("습관 생성하기", style: TextStyle(color: Colors.white)),
               ),
-              child: Text("습관 생성하기", style: TextStyle(color: Colors.white)),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
@@ -284,5 +305,23 @@ class _DevelopNewHabitScreenState extends State<DevelopNewHabitScreen> {
         );
       },
     );
+  }
+
+  Future<String?> translateToEnglish(String koreanText) async {
+    final url = Uri.parse(
+      "https://api.mymemory.translated.net/get?q=$koreanText&langpair=ko|en",
+    );
+    try {
+      final response = await http.get(url);
+      if (response.statusCode == 200) {
+        final Map<String, dynamic> data = json.decode(response.body);
+        String translatedText = data['responseData']['translatedText'];
+        return translatedText.toLowerCase(); // 첫 글자가 대문자로 시작함 → 소문자로 변경
+      }
+    } catch (e) {
+      debugPrint("translate error: $e");
+    }
+    // 통신 실패나 에러가 난 경우 null 반환
+    return null;
   }
 }
